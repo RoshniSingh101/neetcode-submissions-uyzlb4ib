6b483b0f767b4ad1452ecb16323035eb22f23422
RoshniSingh101 <108078575+RoshniSingh101@users.noreply.github.com>
2026-04-15 15:37:14 -0400

    Add: decode-ways - submission-1

---
+++ Data Structures & Algorithms/decode-ways/submission-1.py
@@ -0,0 +1,38 @@
+class Solution:
+    def numDecodings(self, s: str) -> int:
+        dp = { len(s) : 1 }
+        # DP
+        for i in range(len(s) - 1, -1, -1):
+            if s[i] == "0":
+                dp[i] = 0
+            else:
+                dp[i] = dp[i+1]
+            
+            if i + 1 < len(s) and (s[i] == "1" or
+                 s[i] == "2" and s[i+1] in "0123456"):
+                dp[i] += dp[i+2]
+
+        return dp[0]
+        
+        # # recursive
+
+        # def dfs(i):
+        #     # base case
+        #     # i in cache
+        #     # or reached end of string
+        #     if i in dp:
+        #         return dp[i]
+        #     # if not at end, bad base case
+        #     if s[i] == "0":
+        #         return 0
+            
+        #     res = dfs(i + 1)
+        #     # check if index in bounds
+        #     # if starts with a 1, num can be 10-19
+        #     # if starts with a 2, num can be 20-26
+        #     if i + 1 < len(s) and (s[i] == "1" or
+        #         s[i] == "2" and s[i+1] in "0123456"):
+        #         res += dfs(i + 2) # for double digit values
+        #     dp[i] = res # cache
+        #     return res
+        # return dfs(0)
